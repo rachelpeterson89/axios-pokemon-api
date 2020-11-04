8908c65f4107e935b6d89365fb872853c425c41a
Rachel Peterson <rachelp5446@gmail.com>
2020-11-04 16:40:11 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import DisplayPokemon from './components/DisplayPokemon';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 
 function App() {
+
+  const [ pokemon, setPokemon ] = useState(null);
+
+  const getPokemon = () => {
+    
+      axios.get("https://pokeapi.co/api/v2/pokemon?limit=807")
+        .then(res =>{
+          // console.log(res)
+          // console.log(res.data.results)
+          setPokemon(res.data.results)
+        })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="my-5 mx-auto">
+
+      <div className="d-flex justify-content-center">
+        <button 
+          className="btn btn-danger"
+          onClick={ getPokemon }
+        >Catch 'Em All</button>
+      </div>
+    
+      { pokemon ?
+        pokemon.map((p,i) => {
+          return <DisplayPokemon
+                    pokemon={p}
+                    key={i}
+                />
+        }) : <div></div>
+      }
+
     </div>
   );
 }
